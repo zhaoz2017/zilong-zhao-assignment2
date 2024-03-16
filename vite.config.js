39d40ccba9e0cce285d4270354b12c41b00ee6af
zilongzhao <zhao.zilo@northeastern.edu>
2024-03-16 05:45:21 -0700

    modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // 设置输出目录为'build'
+  },
 })
